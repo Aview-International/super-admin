14d5badc79c2d2d3ce4dff111fe60c44d859d5b2
victor <vicopem@gmail.com>
2024-04-03 16:10:57 -0400

    Feat: Subtitling and Marketplace

--- components/subtitling/TimelineSlider.js
+++ components/subtitling/TimelineSlider.js
@@ -1,6 +1,5 @@
-import React, { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import styles from './TimelineSlider.module.css';
-import play from '../../public/img/icons/play-white.svg';
 import Image from 'next/image';
 import play_black from '../../public/img/icons/play-black.svg';
 import pause_black from '../../public/img/icons/pause-black.svg';
@@ -68,17 +67,25 @@ const TimelineSlider = ({ videoRef, hiddenVideoRef }) => {
     const capture = () => {
       if (currentTime <= duration && captures.length < 20) {
         const canvas = document.createElement('canvas');
-        canvas.width =  100;
+        canvas.width = 100;
         canvas.height = 90;
         const context = canvas.getContext('2d');
-        context.drawImage(hiddenVideoRef.current, 0, 0, canvas.width, canvas.height);
+        context.drawImage(
+          hiddenVideoRef.current,
+          0,
+          0,
+          canvas.width,
+          canvas.height
+        );
         captures.push(canvas);
         currentTime += interval;
         if (currentTime <= duration && captures.length < 20) {
           hiddenVideoRef.current.currentTime = currentTime;
         } else {
           setCanvases(captures);
-          canvasRefs.current = captures.map((_, i) => canvasRefs.current[i] || React.createRef());
+          canvasRefs.current = captures.map(
+            (_, i) => canvasRefs.current[i] || React.createRef()
+          );
         }
       }
     };
@@ -94,7 +101,10 @@ const TimelineSlider = ({ videoRef, hiddenVideoRef }) => {
 
     return () => {
       if (hiddenVideoRef.current) {
-        hiddenVideoRef.current.removeEventListener('loadeddata', captureThumbnails);
+        hiddenVideoRef.current.removeEventListener(
+          'loadeddata',
+          captureThumbnails
+        );
         hiddenVideoRef.current.removeEventListener('seeked', capture);
       }
     };
@@ -109,7 +119,7 @@ const TimelineSlider = ({ videoRef, hiddenVideoRef }) => {
   }, [canvases]);
 
   const formatTime = (seconds) => {
-    const milliseconds = seconds % 1 * 1000;
+    const milliseconds = (seconds % 1) * 1000;
     const date = new Date(0);
     date.setSeconds(Math.floor(seconds));
     date.setMilliseconds(milliseconds);
@@ -121,63 +131,69 @@ const TimelineSlider = ({ videoRef, hiddenVideoRef }) => {
     return (currentTime / max) * 100 + '%';
   };
 
-    return ( 
+  return (
     <div className="w-full pl-[20px] pr-[20px]">
-    <div className={styles.timeline_container}>
-      <div className="flex justify-center">
-        <div className="flex flex-row items-center">
-          <div className={`h-[24px] w-[24px] bg-white rounded-full mr-s1 ${isPlaying ? "": "pl-[2px] pt-[1px]"}`}><Image src={isPlaying ? pause_black:play_black} alt="" width={24} height={24} onClick={togglePlayPause}/></div>
-          <div className={styles.time_indicator}>
-            <span>{formatTime(currentTime)}</span> | <span className="text-white text-opacity-75">{formatTime(duration)}</span>
+      <div className={styles.timeline_container}>
+        <div className="flex justify-center">
+          <div className="flex flex-row items-center">
+            <div
+              className={`mr-s1 h-[24px] w-[24px] rounded-full bg-white ${
+                isPlaying ? '' : 'pl-[2px] pt-[1px]'
+              }`}
+            >
+              <Image
+                src={isPlaying ? pause_black : play_black}
+                alt=""
+                width={24}
+                height={24}
+                onClick={togglePlayPause}
+              />
+            </div>
+            <div className={styles.time_indicator}>
+              <span>{formatTime(currentTime)}</span> |{' '}
+              <span className="text-white text-opacity-75">
+                {formatTime(duration)}
+              </span>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="relative w-full">
-        <input
-          type="range"
-          min="0"
-          max={duration}
-          value={currentTime}
-          className={styles.timeline_slider}
-          onInput={handleTimeChange}
-          step="0.001"
-        />
-
-        <div className={styles.ticks}>
-          {[...Array(80)].map((_, i) => (
-            <span key={i} className={styles.tick}></span>
-          ))}
-        </div>
-      </div>
-        <div className="w-full pl-[5px] pr-[7px]">
-          <div className="relative">
-          <div
-            className={styles.vertical_bar}
-            style={{ left: verticalBarPosition() }}
+        <div className="relative w-full">
+          <input
+            type="range"
+            min="0"
+            max={duration}
+            value={currentTime}
+            className={styles.timeline_slider}
+            onInput={handleTimeChange}
+            step="0.001"
           />
-          <div className={styles.thumbnail_container}>
-            {canvases.map((_, index) => (
-              <div key={index} ref={canvasRefs.current[index]} className="w-5% h-[56px] overflow-hidden">
-              </div>
+
+          <div className={styles.ticks}>
+            {[...Array(80)].map((_, i) => (
+              <span key={i} className={styles.tick}></span>
             ))}
           </div>
+        </div>
+        <div className="w-full pl-[5px] pr-[7px]">
+          <div className="relative">
+            <div
+              className={styles.vertical_bar}
+              style={{ left: verticalBarPosition() }}
+            />
+            <div className={styles.thumbnail_container}>
+              {canvases.map((_, index) => (
+                <div
+                  key={index}
+                  ref={canvasRefs.current[index]}
+                  className="w-5% h-[56px] overflow-hidden"
+                ></div>
+              ))}
+            </div>
           </div>
         </div>
-        
-    </div>
+      </div>
     </div>
   );
 };
 
 export default TimelineSlider;
-
-
-
-
-
-
-
-
-
-
-
