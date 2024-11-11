046ed11f22dcc1a90b84631a85c38d50592da97d
victor <vicopem@gmail.com>
2024-11-11 11:56:35 -0500

    Fix: Logging error

--- pages/login/index.js
+++ pages/login/index.js
@@ -25,7 +25,9 @@ const Login = () => {
         Cookies.set('session', res._tokenResponse.idToken, {
           sameSite: 'Strict',
         });
+        console.log(res);
         const user = authStatus(res._tokenResponse.idToken);
+        console.log(user);
         if (user && user.data.accountVerifiedByAview) router.push('/dashboard');
         else throw Error;
       }
